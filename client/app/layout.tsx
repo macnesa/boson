d2b8386dad30c607fae705da9e0853b7ee25ca7a
mmacnesa <mmacnesa@gmail.com>
2025-10-14 10:37:11 +0700

    feat: splitted directory

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "elysium",
+  title: "Boson | Social Media Agency",
   description: "elysium company website",
 };
 
